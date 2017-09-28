2ccbef262c5aed28e73ddb345b78fdb27d15b8ea
Hardik Sharma <hardiksharmax@gmail.com>
2017-09-28 14:46:44 +0530

    Create index.cpp

---
+++ index.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main() {
+    int n,size;
+    std::cin>>n;
+  
+    while(n>0)
+    {
+    	 string res="yes";
+        std::cin>>size;
+        int arr[size];
+        for(int i=0;i<size;i++)
+        {
+            std::cin>>arr[i];
+        }
+        for(int i=0;i<size/2;i++)
+        {
+            if(arr[i]==0 || arr[i]!=arr[size-i-1])
+            {
+                res="no";
+                break;
+            }
+            
+        }
+        cout<<res;
+        n--;
+        
+    }
+   
+    
+}
